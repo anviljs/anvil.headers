d7f308b012d1667dbd2d393203d98b1384edcbe9
Alex Robson <asrobson@gmail.com>
2012-10-31 21:35:35 -0500

    Changing up how header plugin works. Should be backwards compatible with simplest use cases. Please see readme

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 ## Anvil Headers Plugin
 
-This plugin is a core component of anvil and is required to function as expected.
+This plugin enables tokenized header files to get prepended after minimization steps (and also saves you from having to type the same copyright crap at the top of each file).
 
 ## Installation
 
 anvil will install this plugin during post-install.
+
+## Conventions
+
+### File Name
+This plugin assume that your header files will be named header and that the extension of the header file will control which kinds of files the header applies to.
+
+### Top Level Headers
+By convention, a header file at the very top of your repository will get applied to all files in your source tree that have a matching extension.
+
+### Location
+After that, header files within a directory will apply to all files found in that directory and 'override' any headers found at a higher level of the source tree hierarchy.
+
+### File specific headers
+If you need to target a specific file you can provide the following configuration:
+
+	"anvil.headers": {
+		"headers": {
+			"relative/path/to/your/file.ext": "relative/path/to/your/header.ext"
+		}
+	}
--- src/headers.js
+++ src/headers.js
@@ -1,28 +1,41 @@
+var minimatch = require( "minimatch" ),
+	path = require( "path" );
+
 module.exports = function( _, anvil ) {
 	return anvil.plugin( {
 		name: "anvil.headers",
 		activities: [ "identify", "push" ],
 		dependencies: [ "fileLoader" ],
-		commander: [
-			[ "--header [value]", "provide header plugin with the name of header files (minus extension)", "header" ]
-		],
-		headers: {},
 		config: {
-			header: ""
+			header: "header",
+			headers: {}
 		},
 
 		configure: function( config, command, done ) {
 			var self = this,
 				outputPlugin = anvil.plugins[ "anvil.output" ];
-			this.headerFileName = command.header || "header";
 			if( outputPlugin ) {
 				outputPlugin.dependencies.push( "anvil.headers" );
 			}
-			_.each( self.getHeaders( anvil.project.files ), function( file ) {
-				file.noCopy = true;
-				self.headers[ file.extension() ] = file;
-			} );
 			done();
+		},
+
+		findHeaderFiles: function() {
+			var self = this;
+			_.each( this.config.headers, function( file, pattern ) {
+				if( _.isString( file ) ) {
+					var match = _.find(
+						anvil.project.files,
+						function( f ) {
+							var p = self.getRelativeFilePath( f ).replace( /^\/|^\\/, "" );
+							return file === p;
+						} );
+					if( match ) {
+						match.noCopy = true;
+						self.config.headers[ pattern ] = match;
+					}
+				}
+			} );
 		},
 
 		getHeaders: function( files ) {
@@ -30,67 +43,122 @@ module.exports = function( _, anvil ) {
 			return _.filter( files, function( file ) {
 				var full = file.name,
 					limited = full.replace( file.extension(), "" );
-				return limited === self.headerFileName;
+				return limited === self.config.header;
 			} );
 		},
 
 		getOutput: function() {
 			var self = this,
-				lookup = {
-					extensions: []
-				},
-				files = _.filter( anvil.project.files, function( file ) {
-					var extension = file.extension(),
-						hasHeader = _.any( self.headers, function( value, key ) {
-							return key === extension;
-						} );
-					return file.dependents.length === 0 && !file.noCopy && hasHeader;
-				} );
-			_.each( files, function( file ) {
-				var extension = file.extension();
-				lookup.extensions.push( extension );
-				if( lookup[ extension ] ) {
-					lookup[ extension ].files.push( file );
-				} else {
-					lookup[ extension ] = {
-						header: self.headers[ extension ],
-						files: [ file ]
+				lookup = {},
+				list = [];
+			_.each( anvil.project.files, function( file ) {
+				if( !file.noCopy ) {
+					var filePath = self.getRelativeFilePath( file );
+					lookup[ filePath ] = {
+						file: file,
+						headerDepth: -1,
+						header: {}
 					};
+					list.push( filePath );
 				}
 			} );
-			lookup.extensions = _.uniq( lookup.extensions );
+			_.each( this.config.headers, function( header, pattern ) {
+				var matches = minimatch.match( list, pattern, {} ),
+					headerDepth = header.relativePath.split( /\\|\//g ).length,
+					patternFile = path.basename( pattern );
+				_.each( matches, function( fileKey ) {
+					var file = lookup[ fileKey ];
+					if( headerDepth > file.headerDepth ) {
+						file.headerDepth = headerDepth;
+						file.header = header;
+					} else if( patternFile === file.name ) {
+						file.headerDepth = 1000000;
+						file.header = header;
+					}
+				} );
+			} );
 			return lookup;
+		},
+
+		getHeaderPattern: function( file ) {
+			return path.join(
+				file.relativePath.replace( /^\/|^\\/, ""),
+				"**",
+				"*" + file.extension() );
+		},
+
+		getRelativeFilePath: function( file ) {
+			return anvil.fs.buildPath( [
+					file.relativePath.replace( /^\/|^\\/, "" ),
+					file.name
+				] );
+		},
+
+		identify: function( done ) {
+			var self = this;
+			if( !_.isEmpty( this.config.headers ) ) {
+				this.findHeaderFiles();
+			}
+			_.each( self.getHeaders( anvil.project.files ), function( file ) {
+				file.noCopy = true;
+				var pattern = self.getHeaderPattern( file );
+				if( !self.config.headers[ pattern ] ) {
+					self.config.headers[ pattern ] = file;
+				}
+			} );
+			anvil.fs.getFiles( "./", anvil.config.working, function( files, directories ) {
+				if( files.length > 0 ) {
+					_.each( self.getHeaders( files ), function( file ) {
+						file.noCopy = true;
+						var pattern = self.getHeaderPattern( file );
+						self.config.headers[ pattern ] = file;
+						anvil.project.files.push( file );
+					} );
+					done();
+				} else {
+					done();
+				}
+			}, [], 0 );
+		},
+
+		place: function( done ) {
+			var self = this,
+				files = self.getOutput(),
+				lookup = {},
+				list;
+			_.each( files, function( file, fileName ) {
+				if( file.header ) {
+					if( lookup[ file.header.fullPath ] ) {
+						lookup[ file.header.fullPath ].files.push( file.file );
+					} else if( !_.isEmpty( file.header ) ) {
+						lookup[ file.header.fullPath ] = {
+							file: file.header,
+							files: [ file.file ]
+						};
+					}
+				}
+			} );
+			list = _.map( lookup, function( header ) { return header; } );
+			anvil.scheduler.parallel( list, function( header, done ) {
+				anvil.fs.read( [ header.file.workingPath, header.file.name ], function( headerContent ) {
+					var prepend = function( content, done ) {
+						done( headerContent + "\n" + content );
+					};
+					anvil.scheduler.parallel( header.files, function( file, done ) {
+						var target = [ file.workingPath, file.name ];
+						anvil.fs.transform( target, prepend, target, done );
+					}, done );
+				} );
+				}, function() { done(); }
+			);
 		},
 
 		run: function( done, activity ) {
 			var self = this;
 			if( activity === "identify" ) {
-				anvil.fs.getFiles( "./", anvil.config.working, function( files, directories ) {
-					if( files.length > 0 ) {
-						_.each( self.getHeaders( files ), function( file ) {
-							self.headers[ file.extension() ] = file;
-							file.noCopy = true;
-							anvil.project.files.push( file );
-						} );
-						done();
-					} else {
-						done();
-					}
-				}, [], 0 );
+				this.identify( done );
 			} else {
-				var lookup = self.getOutput();
-				anvil.scheduler.parallel( lookup.extensions, function( extension, done ) {
-					var set = lookup[ extension ];
-					anvil.fs.read( [ set.header.workingPath, set.header.name ], function( header ) {
-						var prepend = function( content, done ) {
-							done( header + "\n" + content );
-						};
-						anvil.scheduler.parallel( set.files, function( file, done ) {
-							var target = [ file.workingPath, file.name ];
-							anvil.fs.transform( target, prepend, target, done );
-						}, done );
-					} );
-				}, function() { done(); } );
+				this.place( done );
 			}
 		}
 	} );
